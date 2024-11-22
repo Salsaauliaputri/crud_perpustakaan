bd4679ac80f87fe113a1780c65c4d945fa45215e
Your Name <you@example.com>
2024-11-22 10:28:10 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:crud_perpustakaan/home_page.dart';
+import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Supabase.initialize(
+    url: 'https://anprhqtxfqymojnaqopt.supabase.co',
+    anonKey:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFucHJocXR4ZnF5bW9qbmFxb3B0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzE3MjY3ODUsImV4cCI6MjA0NzMwMjc4NX0.pLEl1kQnXYWZzeL12W03YIUbY7ZS4tveETuWE7MIxOw',
+  );
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Digital Library',
+      home: BookListPage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
